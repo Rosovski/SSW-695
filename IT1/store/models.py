2ae52d3bc39c78af05e55dde67fb0f2655ce7590
Vineet Singh <vsingh17@stevens.edu>
2020-03-26 14:36:42 -0400

    added mobile field and choices to select store types

--- IT1/store/models.py
+++ IT1/store/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 
 class Store(models.Model):
-    name = models.CharField(max_length=120)
+    STORE_TYPES = (
+        ('deli', 'Deli'),
+        ('cafe', 'Cafe'),
+        ('electronics', 'Electronics'),
+        ('ice cream parlour', 'Ice cream parlour'),
+        ('restaurant', 'Restaurant'),
+        ('other', 'Other'),
+    )
+    owner_name = models.CharField(max_length=120)
     business_name = models.CharField(max_length=250)
     mobile = models.PositiveIntegerField()
     email = models.EmailField(max_length=100)
@@ -13,4 +21,6 @@ class Store(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=20, default='United States')
     business_type = models.CharField(max_length=20)
+    store_type = models.CharField(
+        max_length=100, choices=STORE_TYPES, default='deli')
     registration_date = models.DateField()
