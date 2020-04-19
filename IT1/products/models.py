8f8262a75bf09dd86874d849086b4c19958510da
Vineet Singh <vsingh17@stevens.edu>
2020-04-19 18:46:59 -0400

    Store models used in Products app.

--- IT1/products/models.py
+++ IT1/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+from store.models import Store
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
@@ -10,7 +11,9 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=20)
     category = models.TextField(blank=True, null=True)
     featured = models.BooleanField(default=False)
-    store = models.TextField(default="Unknown Store")
+    # store = models.TextField(default="Unknown Store")
+
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse("product-detail", kwargs={"id": self.id})
